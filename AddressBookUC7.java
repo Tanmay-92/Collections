48020aa33f7a79979bbe625081ed6dd9f9c4665b
Tanmay-92 <tanmaykumallick@gmail.com>
2021-08-15 18:55:02 +0530

    Adding the UC-7

---
+++ AddressBookUC7.java
@@ -0,0 +1,112 @@
+package Collections;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class AddressBookUC7 {
+	
+	public static Scanner sc = new Scanner(System.in);
+    public ArrayList<Contacts1>contactList = new ArrayList<>();
+
+    public void addContactDetails()
+    {
+        System.out.println("Enter the Details of ContactDetails");
+        System.out.println("Enter the first name");
+        String firstName = sc.next();
+        if (checkDuplicate(firstName)) 
+        {
+            System.out.println("Person is already exist");
+        }
+        else 
+        {
+            System.out.println("Enter the Last name");
+            String lastName = sc.next();
+            System.out.println("Enter the Address");
+            String address = sc.next();
+            System.out.println("Enter the City");
+            String city = sc.next();
+            System.out.println("Enter the State");
+            String state = sc.next();
+            System.out.println("Enter the email");
+            String email = sc.next();
+            System.out.println("Enter the ZipCode");
+            String zip = sc.next();
+            System.out.println("Enter the contact number...");
+            String phoneNumber = sc.next();
+            Contacts1 contactofPerson = new Contacts1(firstName, lastName, address, city, state, email, phoneNumber, zip);
+            contactList.add(contactofPerson);
+
+        }
+    }
+
+    public boolean editContactDetails(String Name) 
+    {
+        int flag = 0;
+        for(Contacts1 contact: contactList)
+        {
+            if(contact.getFirstName().equals(Name))
+            {
+        
+				Scanner sc = new Scanner(System.in);
+                System.out.println("Enter Address: ");
+                String address = sc.next();
+                contact.setAddress(address);
+                System.out.println("Enter City: ");
+                String city = sc.next();
+                contact.setCity(city);
+                System.out.println("Enter State: ");
+                String state = sc.next();
+                contact.setState(state);
+                System.out.println("Enter Email: ");
+                String email = sc.next();
+                contact.setEmail(email);
+                System.out.println("Enter Phone Number:");
+                String phoneNumber = sc.next();
+                contact.setPhoneNumber(phoneNumber);
+                System.out.println("Enter Zip Code: ");
+                String zip = sc.next();
+                contact.setZip(zip);
+                flag = 1;
+            }
+        }
+        return flag == 1;
+    }
+      public boolean deleteContact(String name) 
+      {
+        int flag = 0;
+        for(Contacts1 contact: contactList)
+        {
+            if(contact.getFirstName().equals(name))
+            {
+                contactList.remove(contact);
+                flag = 1;
+                break;
+            }
+        }
+          return flag == 1;
+    }
+    public boolean checkDuplicate(String fname)
+    {
+        int flag=0;
+        for (Contacts1 p: contactList)
+        {
+            if (p.getLastName().equals(fname))
+            {
+                flag=1;
+                break;
+            }
+        }
+        return flag == 1;
+    }
+
+
+
+      public void display() 
+      {
+  		for (Contacts1 person : contactList)				
+  			System.out.println(person);
+  	  } 
+
+
+}
